12047cf049daf1e630e7fee20b2c19c1a361e59e
Jin <jjetwong@gmail.com>
2023-09-18 14:24:44 +0100

    Added menu pages to website

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        public: resolve(__dirname, "public/menu-1.html"),
+        public: resolve(
+          __dirname,
+          "public/menu-1.html",
+          "public/menu-2.html",
+          "public/menu-3.html",
+          "public/menu-4.html"
+        ),
       },
     },
   },
