479f1ec5c390937bb681a5a0909b070c414e0637
Jin <jjetwong@gmail.com>
2023-09-19 16:56:23 +0100

    Fixed vite multiple pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        public: resolve(__dirname, "public/menu-1.html", "public/menu-2.html"),
+        menu1: resolve(__dirname, "public/menu-1.html"),
+        menu2: resolve(__dirname, "public/menu-2.html"),
+        menu3: resolve(__dirname, "public/menu-3.html"),
+        menu4: resolve(__dirname, "public/menu-4.html"),
       },
     },
   },
